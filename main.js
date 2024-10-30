0e7808f480a6f5cb0f14257a09a7fbb7d20c5f54
Ngoc Ngo <139940405+angocit@users.noreply.github.com>
2024-10-30 16:01:19 +0700

    B1

--- main.js
+++ main.js
@@ -1,38 +1,35 @@
 // Destructuring: Trích xuất giá trị từ mảng hoặc object và gán vào biến cụ thể nào đó
 // Mảng
-
-// const a = [5,10,30,9,6,8,100]
+// const a = [5,7,9,2,10,50,30,100]
 // // const b = a[0]
 // // const c = a[1]
-// // const [b,,,c] = a
-// // console.log(b);
-
-// // console.log(c)
-// // kết hợp spread để lấy phần tử còn lại của mảng
+// // console.log(b,c);
+// // const [,,,,c] =a
+// // console.log(c);
+// // kết hợp với spread để lấy phần tử còn lại của mảng 
 // const [b,c,...d] = a
-// console.log(b);
-// console.log(c);
+// console.log(b,c);
 // console.log(d);
-// Object
-const name = "sdfasdfsad"
+// Object 
 const person = {
     name:"Ngọc",
-    age:20,
-    address: "Thái Nguyên"
+    age: 20,
+    address:"Thái Nguyên"
 }
-// const name = person.name;
+// const name = person.name 
 // const age = person.age
-// const {name,age} = person
-console.log(name);
-// lưu ý: biến destructuring phải trùng với tên thuộc tính
-// muốn đặt khác thì làm như sau: 
-
-const {name:ten,age} = person
-console.log(ten);
+const {address,age} = person
+console.log(address,age);
+// lưu ý: Tên biên trùng với tên thuộc tính, nếu muốn tên
+// biến khác với tên thuộc tính thì làm như sau: 
+const {name:ten,age:tuoi} = person
+console.log(ten,tuoi);
 // Sử dụng destructuring trong hàm
-function display({name,age}){
-    console.log(`Tên là: ${name} `);
-    console.log(`Tuoi: ${age}`);   
-    
+// function display(ten,tuoi){
+//     console.log('Tên là: '+ten+'. Tuổi:'+tuoi);   
+// }
+// display(person.name,person.age)
+function display({name:ten,age:tuoi}){
+    console.log('Tên là: '+ten+'. Tuổi:'+tuoi);   
 }
 display(person)
