76834c797d0eb30d4e94e5e893511d0b37714c14
unknown <angocit@gmail.com>
2025-11-26 13:26:17 +0700

    b11: UD Xử lý bất đồng bộ

--- lession7.js
+++ lession7.js
@@ -84,20 +84,42 @@
 //     }
 // }
 // GetAllProduct()
-const Login = async ()=>{
-    const user = {
-        "username": "string",
-        "password": "string"
-        }
+// const Login = async ()=>{
+//     const user =  { username: 'john_doe', password: 'pass123' }
+//     try {
+//         const response = await fetch("https://fakestoreapi.com/auth/login",{
+//             method:'POST',
+//             body:JSON.stringify(user),
+//             headers: {'Content-type':'Application/json'}
+//         })
+//         const data=await response.json()
+//         console.log(data);        
+//     } catch (error) {
+//         console.log('Lỗi',error);        
+//     }
+// }
+// Login() // Promise reject 
+const GetAllProduct = async()=>{
     try {
-        const response = await fetch("https://fakestoreapi.com/auth/login",{
-            method:'POST',
-            body:user
-        })
-        const data=await response.json()
-        console.log(data);        
+    const response = await fetch("https://fakestoreapi.com/products")
+    const data = await response.json()
+    console.log(data);
+    // Truy cập vào DOM
+    const productElm = document.querySelector(".listproduct")
+    // Duyệt mảng
+    data.forEach(({title,image,price})=>{
+        // Tạo item div
+        const div = document.createElement('div')
+        div.innerHTML=`
+            <img src="${image}"/>
+            <h3>${title}</h3>
+            <span>${price}</span>
+        `
+        // Chèn vào DOM
+        productElm.appendChild(div)
+    })
     } catch (error) {
-        console.log('Lỗi',error);        
+        console.log(error);        
     }
 }
-Login()
+GetAllProduct()
