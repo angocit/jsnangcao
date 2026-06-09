b99e785d42c7672ba31bc62cabcd9029f8c4cae9
angocit <angocit@gmail.com>
2026-06-09 13:26:09 +0700

    b13: export default

--- module.js
+++ module.js
@@ -40,4 +40,4 @@ const handleDelete = async(id)=>{
                 
             }
         }
-export {handleDelete,GetProducts}
+export default GetProducts
--- product.js
+++ product.js
@@ -1,4 +1,4 @@
-import * as CRUD from "./module.js";
+import dssp from "./module.js";
 const tbody1 = document.getElementById("table1")
 const tbody2 = document.getElementById("table2")
-CRUD.GetProducts(tbody2)
+dssp(tbody2)
